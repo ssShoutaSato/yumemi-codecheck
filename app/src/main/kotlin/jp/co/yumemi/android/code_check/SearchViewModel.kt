4b63b6c92bc2ed02f4c992c3180865be92c07da2
S.Shouta <ss.shouta.sato@gmail.com>
2022-09-30 06:48:38 +0900

    Fixed how to set the last search date.

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchViewModel.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/SearchViewModel.kt
@@ -66,7 +66,7 @@ class SearchViewModel : ViewModel() {
                 )
             }
 
-            lastSearchDate = Date()
+            lastSearchDate.time = Date().time
 
             return@async list.toList()
         }.await()
