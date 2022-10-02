990f1231f90e2238ebd5a80a0638d38237dab096
S.Shouta <ss.shouta.sato@gmail.com>
2022-10-02 23:45:01 +0900

    Removed adapter class from fragment and created a new class.

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/SearchFragment.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/ui/search/SearchFragment.kt
@@ -4,11 +4,8 @@
 package jp.co.yumemi.android.code_check.ui.search
 
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.view.inputmethod.EditorInfo
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.*
@@ -65,60 +62,5 @@ class SearchFragment : Fragment(R.layout.fragment_search) {
         val action =
             SearchFragmentDirections.actionRepositoriesFragmentToRepositoryFragment(repositoryInformation = repositoryInformation)
         findNavController().navigate(action)
-    }
-}
-
-val diffUtil = object : DiffUtil.ItemCallback<RepositoryInformation>() {
-    override fun areItemsTheSame(oldItem: RepositoryInformation, newItem: RepositoryInformation): Boolean {
-        return oldItem.name == newItem.name
-    }
-
-    override fun areContentsTheSame(oldItem: RepositoryInformation, newItem: RepositoryInformation): Boolean {
-        return oldItem == newItem
-    }
-}
-
-/**
- * This adapter for displaying a list of search results.
- *
- * Notify that an item has been clicked with [OnItemClickListener.itemClick].
- *
- * @param itemClickListener The listener for notifying that an item has been clicked.
- */
-class CustomAdapter(private val itemClickListener: OnItemClickListener) :
-    ListAdapter<RepositoryInformation, CustomAdapter.ViewHolder>(diffUtil) {
-    /**
-     * [ListAdapter.onCreateViewHolder]
-     */
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.layout_item, parent, false)
-        return ViewHolder(view)
-    }
-
-    /**
-     * [ListAdapter.onBindViewHolder]
-     */
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = getItem(position)
-        val view = holder.itemView.findViewById<View>(R.id.repositoryNameView)
-        if (view is TextView) {
-            view.text = item.name
-        }
-        holder.itemView.setOnClickListener { itemClickListener.onItemClicked(item) }
-    }
-
-    /**
-     * This class for holding the view displayed in the list.
-     */
-    class ViewHolder(view: View) : RecyclerView.ViewHolder(view)
-
-    /**
-     * The listener for notifying that an item has been clicked.
-     */
-    interface OnItemClickListener {
-        /**
-         * Notify that item is clicked.
-         */
-        fun onItemClicked(item: RepositoryInformation)
     }
 }
